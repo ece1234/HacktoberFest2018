dae55c4c52c899bea47d13ce6f0e1deb5d8452b3
ece1234 <44495883+ece1234@users.noreply.github.com>
2019-10-23 16:24:59 +0300

    Update CodeKAL Sevgisi.py
    codekal sevgimi paylaştım

--- Python/CodeKAL sevgisi.py
+++ Python/CodeKAL sevgisi.py
@@ -7,3 +7,5 @@ print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
 
 #YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
 # başına "#" (yorum işareti) koyarak düşüncelerinizi kodun sonuna ekleyiniz.
+print("I love CodeKAL so much")
+print("I'm so happy to be a part of CodeKAL")
